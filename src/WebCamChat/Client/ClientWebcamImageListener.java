90c7eb3b117d1dc15839011b8fcced9fa7c9ad79
ProtoBrunch <robin.berberat@gmail.com>
2017-03-31 15:26:17 +0200

    added empty

--- src/WebCamChat/Client/ClientWebcamImageListener.java
+++ src/WebCamChat/Client/ClientWebcamImageListener.java
@@ -10,6 +10,7 @@ import java.net.Socket;
 public class ClientWebcamImageListener extends Thread {
     Socket server;
     DataInputStream webcamFromServer;
+    byte[] empty = null;
 
     ClientWebcamImageListener(Socket server){
         try{
@@ -23,22 +24,23 @@ public class ClientWebcamImageListener extends Thread {
     public void run(){
         WebcamChatGui webcamPanel = new WebcamChatGui();
         webcamPanel.setComponents();
-        while(true){
+        while(true) {
             try {
                 int length = webcamFromServer.readInt();
-                if(length > 0) {
-                    byte[] webcamIn = new byte[length];
+                if (length > 0) {
+                    byte[] webcamIn = empty;
+                    webcamIn = new byte[length];
                     webcamFromServer.readFully(webcamIn, 0, length);
                     webcamPanel.changeWebcamImageIcon(new ServerInputProcessor().byteArrayToImageIcon(webcamIn, length));
+                    webcamIn = empty;
                 }
             } catch (IOException e) {
                 e.printStackTrace();
-            } catch (NullPointerException e){
+            } catch (NullPointerException e) {
 
-            }catch(OutOfMemoryError e){
+            } catch (OutOfMemoryError e) {
 
             }
-
         }
     }
 }
